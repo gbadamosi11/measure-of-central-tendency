fb3e30d918e9afbe1dddbdea0be6d555cbb5ae56
Adekeye Adeniyi <adekeyeimmanuel@gmail.com>
2022-02-23 11:58:08 +0100

    displaying calculated mean, mode and median result

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 
+
+// Button
+let button = document.querySelector('#calculate');
+
+// Input
+let numbers = document.querySelector('#numbers');
+
+// Display Result 
+let meanResult  = document.querySelector('#mean');
+let medianResult  = document.querySelector('#median');
+let modeResult  = document.querySelector('#mode');
+
+button.addEventListener("click", (e)=>{
+        e.preventDefault();
+
+        // get input value
+        // convert values to an array
+        // map each value to a Number method ( change each value into an integer)
+        let value = Array.from(numbers.value).map(Number);
+        
+        
+        if(value){
+            medianResult.textContent = median(value);
+            modeResult.textContent = mode(value).toString();
+            meanResult.textContent =  mean(value);
+        }
+
+
+        // Clear the input field
+        numbers.value = ' ';
+    }
+)
+
+
 function mean(array){
-    mean = array.reduce((a, b) => a + b, 0);
+    let mean = array.reduce((a, b) => a + b, 0);
     // for(let i = 0; i < array.length; i++){
     //     sum += array[i];
     // }
@@ -35,8 +69,3 @@ function mode(array){
     return modes;
 }
 
-const data = [1,2,2,4];
-
-console.log(mean(data));
-console.log(median(data));
-console.log(mode(data));;
